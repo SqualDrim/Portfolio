064d25dd4d81176baa6d0bf2fd4928d3b4fd65a5
Kilian Breviere <kilianbreviere@MacBook-Air-de-Kilian.local>
2025-04-29 21:04:48 +0200

    deploy

--- src/projets/avalam.tsx
+++ src/projets/avalam.tsx
@@ -5,7 +5,7 @@ const Avalam: React.FC = () => {
   return (
       <div className="content">
         <h1>Algorithme Avalam -</h1>
-        <img className='photo1projet' src='../public/avalam1.png'></img>
+        <img className='photo1projet' src='/avalam1.png'></img>
         <div className="tags">
       <p className="tag">C</p>
       <p className="tag">Gestion de projet</p>
