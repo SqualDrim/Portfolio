49e75217d724c680f2d1f3af99ad76ce07950b85
Kilian Breviere <kilianbreviere@MacBook-Air-de-Kilian.local>
2025-04-29 20:41:56 +0200

    deploy

---
+++ src/projets/web2-2.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css'; // même fichier de style ou un autre
+
+const BackWeb2: React.FC = () => {
+  return (
+    <div className="content">
+
+      <h1>- WheaterFIT</h1>
+    </div>
+  );
+};
+
+export default BackWeb2;
